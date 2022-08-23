acb2b7ae49e68d4d3809a53036289afeb3ec1f5c
Laboratório Informática <labinfo@unifil.br>
2022-08-23 19:12:05 -0300

    Exercicios completos!!

---
+++ exercicios/area.py
@@ -0,0 +1,9 @@
+a = round(float(input()),2)
+b = round(float(input()),2)
+c = round(float(input()),2)
+
+print(f"Area do triangulo retangulo: {round(((a*c)/2),3)}")
+print(f"Area do circulo: {round((3.14159*(c*c)),3)}")
+print(f"Area do trapezio {round((((a*b)*c)/2),3)}")
+print(f"Area do quadrado {round((b*b),3)}")
+print(f"Area do retangulo {round((a*b),3)}")
---
+++ exercicios/salario.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+c = float(input())
+
+print("Numero de Horas trabalhadas: " , a)
+print(f"Salario: {round((b*c),2)}")
